e7f89b1c0ba5b18be30a48185b4ae0981a6d13a4
ArunachalamJegadeesan <inboxofarun@gmail.com>
2020-05-15 21:04:48 +0530

    clean up and unittest issue fix for web

--- catalog-web/src/test/java/com/example/web/controllers/CatalogControllerTest.java
+++ catalog-web/src/test/java/com/example/web/controllers/CatalogControllerTest.java
@@ -16,9 +16,8 @@ import com.example.web.service.WebService;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.when;
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(WebController.class)
+@WebMvcTest(controllers=WebController.class , secure = false)
 public class CatalogControllerTest {
 	
 	@MockBean
